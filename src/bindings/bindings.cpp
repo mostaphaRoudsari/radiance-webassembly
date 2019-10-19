bb0851a93ca00906ee88780fb28a49e950f0f826
Steve Baer <steve@mcneel.com>
2019-10-19 14:17:27 -0700

    one step closer

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -1,37 +1,21 @@
 #include "bindings.h"
 
-#if defined(ON_WASM_COMPILE)
 using namespace emscripten;
 
-void bsdf()
+int bsdf(int a, int b)
 {
-  //double ds[2];
-  //ds[0] = 0.0;
-  //ds[1] = 1.0;
-  //SDsquare2disk(ds, 9, 5);
+  return a + b;
 }
 
 
-void initRadianceBindings(void*)
+void initRadianceBindings()
 {
   function("bsdf", &bsdf);
 }
 
-#endif
 
-
-#if defined(ON_PYTHON_COMPILE)
-namespace py = pybind11;
-PYBIND11_MODULE(_rhino3dm, m){
-  m.doc() = "rhino3dm python package. OpenNURBS wrappers with a RhinoCommon style";
-#endif
-
-#if defined(ON_WASM_COMPILE)
-using namespace emscripten;
-EMSCRIPTEN_BINDINGS(radiance) {
-  void* m = nullptr;
-#endif
-
-  initRadianceBindings(m);
+EMSCRIPTEN_BINDINGS(radiance)
+{
+  initRadianceBindings();
 }
 
--- src/build_javascript.py
+++ src/build_javascript.py
@@ -17,7 +17,7 @@ def system(cmd):
 
 def compilebinaries():
     os.chdir(build_dir)
-    system("emcmake cmake -DCMAKE_CXX_FLAGS=\"-s MODULARIZE=1 -s 'EXPORT_NAME=\\\"radience\\\"'\" ../..")
+    system("emcmake cmake -DCMAKE_CXX_FLAGS=\"-s MODULARIZE=1 -s 'EXPORT_NAME=\\\"radiance\\\"'\" ../..")
     system("make")
     system("make install")
     os.chdir("../..")
